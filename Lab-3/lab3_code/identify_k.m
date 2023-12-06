b6cbd1b9d02b8b66e3bfece6430d6c697c47672a
Jacob Rinderud <66208369+Rinderud@users.noreply.github.com>
2023-12-06 14:49:29 +0100

    Maybe T1 is done?

--- Lab-3/lab3_code/identify_k.m
+++ Lab-3/lab3_code/identify_k.m
@@ -1,10 +1,12 @@
 run('init_quadcopter_model')
 run('init_quadcopter_states')
 run('init_noise_levels')
-addpath ('models')
+addpath ('Lab-3/lab3_code/models')
 %%
 Omega_in.time = (0:inner_h:10)';
-Omega_in.signals.values = ones(length(Omega_in.time),1)*ones(1,4) %You will need to change ones to something better, HINT linspace
+
+Omega_in.signals.values = ones(length(Omega_in.time),1)*ones(1,4);
+%You will need to change ones to something better, HINT linspace
 %%
 disp('starting sim')
 out = sim('omega_input','StopTime', '5')
@@ -15,13 +17,21 @@ clf
 plot(out.p)
 title('Position')
 legend('x','y','z')
+exportgraphics(figure(1), "Lab-3/img/T1_fig1.pdf", "ContentType","vector")
 %% Using Toolbox, estimating from accelerometer.
-%dat1 = TODO;
+%Assuming that the only difference is to not do power of 2
+dat1 = iddata(out.acc.data(:,3)+g,4*out.Omega.data(:,1),inner_h);
+
+%dat2 = iddate(zdotdot + g, 4 * omega.^2, h)
 dat2 = iddata(out.acc.data(:,3)+g,4*out.Omega.data(:,1).^2,inner_h);
-%dat3 = TODO;
-%sys1 =TODO
-sys2 = procest(dat2,'p0') %p0 means zero poles
-%sys3 =TODO
+
+%Same assumptions as on dat1s
+dat3 = iddata(out.acc.data(:,3)+g,4*out.Omega.data(:,1).^3,inner_h);
+
+sys1 = procest(dat1,'p0');
+sys2 = procest(dat2,'p0'); %p0 means zero poles
+sys3 = procest(dat3,'p0');
+
 figure(2)
 clf
 subplot(3,1,1)
@@ -30,5 +40,12 @@ subplot(3,1,2)
 compare(dat2,sys2)
 subplot(3,1,3)
 compare(dat3,sys3)
-%k_est = TODO
+exportgraphics(figure(2), "Lab-3/img/T1_fig2.pdf", "ContentType","vector")
+
+%u = 4 omega^2
+%K_p = (zdotdot + g) / u
+%k_est = K_p * m
+k_ests = (out.acc.data(:,3)+g) .* m ./ (4 * out.Omega.data(:,1).^2);
+% => an array of a k for each datapoint in zdotdot
+k_est = mean(k_ests)
 k
---
+++ Lab-3/report.md
@@ -0,0 +1,9 @@
+# Learning Quadcopter Flight Dynamics
+
+Written by: Jacob Rinderud.
+
+## Task 1
+
+### Results
+
+### Discussion
